85b222f25d230ba603536d41109d15097edf8512
Xavier Georget <xageorget@gmail.com>
2022-09-29 12:04:45 +0200

    solution for the workshop

---
+++ main.js
@@ -0,0 +1,8 @@
+import chalk from "chalk";
+
+console.log(
+  chalk.blue.bgRed.bold("Valentin") +
+    chalk.red.bgBlue.bold("Maeva") +
+    chalk.green.bgYellow.bold("Joryan") +
+    chalk.yellow.bgGray.bold("Enzo")
+);
